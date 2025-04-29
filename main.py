f5ac86f60cc28e152b517783450e402beb279a8f
steveo <steveo@arista.com>
2025-04-28 20:47:28 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+from urllib.request import urlopen
+
+api_key = os.environ["NPR_API_KEY"]
+
+for id_, filename in [(3, 'npr_morning_edition.xml'), (2, 'npr_all_things_considered.xml')]:
+    url = f"https://api.npr.org/query?id={id_}&output=podcast&apiKey={api_key}"
+    with urlopen(url) as response:
+        content = response.read()  # Download as bytes
+    with open(filename, "wb") as file:
+        file.write(content)
